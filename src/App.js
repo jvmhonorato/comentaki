6c0af6afd5e100dafc8d558e695a639da3f348ca
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-10-31 17:48:44 -0300

    Create  project and set FIREBASE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
+import React from 'react'
 import './App.css';
+import firebase from './firebase'
+
+//connect firebase backend
+const ref = firebase.database().ref('test')
+ref.on('value',snapshoot =>{
+  console.log(snapshoot.val())
+})
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app';
+import "firebase/compat/database";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB1rAApiHdky_wQ7cH8orKMKRMmUrwjvKE",
+    authDomain: "comentaki-jvmh.firebaseapp.com",
+    projectId: "comentaki-jvmh",
+    storageBucket: "comentaki-jvmh.appspot.com",
+    messagingSenderId: "916163793623",
+    appId: "1:916163793623:web:4eb2371d7f80b5fc5d9692"
+  };
+
+  firebase.initializeApp(firebaseConfig)
+
+  export default firebase
