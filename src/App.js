3481361f4f08256802fbd021f65fc752d559269d
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-10-31 19:05:20 -0300

    useEffect to clean firebase connection

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import './App.css';
 import firebase from './firebase'
 
-//connect firebase backend
-const ref = firebase.database().ref('test')
-ref.on('value',snapshoot =>{
-  console.log(snapshoot.val())
-})
+const Comments = () => {
+  const [data, setData] = useState({})
+  useEffect(()=> {
+    const ref = firebase.database().ref('test')
+    //turn ON database LINK
+    ref.on('value',snapshoot =>{
+      console.log(snapshoot.val())
+      setData(snapshoot.val())
+    })
+    return () => {
+      //turn OFF database link
+      ref.off()
+    }
+  },[])
+return (
+  <pre>{JSON.stringify(data)}</pre>
+)
+
+} 
 
 
 function App() {
+const [visible, toogle] = useState(true)
+
   return (
     <div >
+      <button onClick={() => toogle(!visible)}>Toogle</button>
+     { visible && <Comments/>}
      
     </div>
   );
