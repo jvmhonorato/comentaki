63824f361af53039ec77f73cecf8f988c430cdb6
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-11-03 18:15:13 -0300

    Finish  manege DisplayName

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,17 +1,45 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 
 import { AuthContext } from "./auth";   
 
+
+//Component to manege input and save data form
+const FormDisplayName=({ displayName, user }) => {
+    const [newDisplayName, setNewDisplayName] = useState(displayName)
+    const onChange = evt => {
+        setNewDisplayName(evt.target.value)
+    }
+    const save = () => {
+        if(newDisplayName !== ''){
+            user.updateProfile({displayName: newDisplayName})
+        }
+    }
+    return (
+        <>
+           <input type='text' value={newDisplayName} onChange={onChange}/>
+           <button onClick={save}>Save display name</button>
+        </>
+    )
+}
+
 const  UserInfo = () => {
 const auth = useContext(AuthContext)
+
+const { displayName } = auth.user
+const alternativeDisplayName = auth.user.email
+const dn = displayName || alternativeDisplayName
+
+
 if (auth.user === null) {
     return null
  }   
- const { displayName } = auth.user
-const [alternativeDisplayName] = auth.user.email.split('@')
 
 return (
-    <p> Olá {displayName || alternativeDisplayName }</p>
+    <>
+       <p> Olá {displayName || alternativeDisplayName }!</p>
+    <FormDisplayName displayName={dn} user={auth.user}/>
+    </>
+   
 )
 }
 export default UserInfo
